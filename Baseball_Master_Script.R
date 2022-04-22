f616b504723647d2cf8ef2d48dcf50f1cee61045
SamSmith2505 <33761671+SamSmith2505@users.noreply.github.com>
2022-04-22 17:22:30 -0600

    Update Baseball_Master_Script.R

--- Baseball_Master_Script.R
+++ Baseball_Master_Script.R
@@ -11,13 +11,16 @@ setwd("~/Documents/Github/MLB_Simulation_Model")
 
 output_filename = paste0("MLB_Simulation_Results_", today(), ".xlsx")
 
-game_iterations = 300
-batter_result_weight = .4
-pitcher_result_weight = .6
+game_iterations = 200
+batter_result_weight = .45
+pitcher_result_weight = .55
 
 dk_score_stdev_weight = 1.5
 dk_salary_constraint = 50000
 draftkings_lineup_count = 20
+
+season_weight_2020 = .2
+season_weight_2021 = .8
 
 start_time = Sys.time()
 
@@ -25,27 +28,25 @@ source("Baseball_Data_Prep.R")
 
 source("Baseball_Pitch_Count_Bullpen_Prep.R")
 
-# while(1==1){
-#   skip = F
-#   tryCatch({
-#source("Baseball_Pull_Live_Lineups.R")
-
-#fwrite(lineup_dt, "today.csv", row.names = F)
-# lineup_dt = lineup_dt[!(game_id %in% c("630894", "630895")),]
-lineup_dt = fread("daily_lineup_df.csv")
-# #lineup_dt[player == "Hyun-Jin Ryu", player := "Hyun Jin Ryu"]
-# #lineup_dt[player == "Yoshi Tsutsugo", player := "REPLACEMENT BATTER"]
-# # # lineup_dt[player == "Joseph Odom", player := "REPLACEMENT BATTER"]
-# # # lineup_dt[player == "Ryan Cordell", player := "REPLACEMENT BATTER"]
-# lineup_dt[player == "_Ronald Acuna Jr.", player := "Ronald Acuna Jr."]
-#lineup_dt[player == "Mike Brosseau", player := "Lourdes Gurriel"]
-# # # # lineup_dt[player == "Trevor Rogers", player := "REPLACEMENT PITCHER"]
-# lineup_dt[player == "Jose Urquidy", player := "REPLACEMENT PITCHER"]
-#lineup_dt[player == "Lance McCullers", player := "Lance McCullers Jr."]
-
+source("Baseball_Pull_Live_Lineups.R")
+#lineup_dt = lineup_dt[!(game_id %in% c(662122,662570,662934)),]
+lineup_dt[player == "Yoshi Tsutsugo", player := "REPLACEMENT BATTER"]
+lineup_dt[player == "Mike Brosseau", player := "REPLACEMENT BATTER"]
+lineup_dt[player == "Adrian Sanchez", player := "REPLACEMENT BATTER"]
+lineup_dt[player == "Jose Barrero", player := "REPLACEMENT BATTER"]
+lineup_dt[player == "Alfonso Rivas", player := "REPLACEMENT BATTER"]
+#lineup_dt[player == "Noah Syndergaard", player := "REPLACEMENT PITCHER"]
+lineup_dt[player == "Drew Ellis", player := "REPLACEMENT BATTER"]
+lineup_dt[player == "Shohei Ohtani" & position == 'Y', position := 1]
+#lineup_dt = fread('daily_lineup_df.csv')
+#lineup_dt[player == "Jose Herrera", player := "REPLACEMENT BATTER"]
 source("Baseball_Game_Simulation_Loop.R")
 
 source("Baseball_Box_Score_Generator.R")
+
+#source("MLB_Bet_Creation.R")
+
+#fwrite(bet_print_dt, "today_bets.csv", row.names = F)
     
     # })
     #   if (skip) {
@@ -54,7 +55,7 @@ source("Baseball_Box_Score_Generator.R")
     # }
 
 source("Baseball_Draftkings_Lineup_Optimizer.R")
-# 
+
 source("Baseball_Random_Functions.R")
 
 output_list = list(
@@ -76,4 +77,8 @@ fwrite(box_daily_win_dt, "win_dt_today.csv", row.names = F)
 end_time = Sys.time()
 
 runtime = end_time - start_time
+
+graph_results("Cleveland Indians")
+
+remove_game(removal_game_id = 6)
 
